5a228f17382bbd119ff36db001832e62d899101f
Dylan Donnell <dylan.donnell@student.griffith.ie>
2025-03-04 22:35:09 +0000

    feat: use template parameter packs for notify-send

--- include/util.h
+++ include/util.h
@@ -1,5 +1,4 @@
 #include <memory>
-#include <stdarg.h>
 #include <stdexcept>
 #include <string>
 #include <unistd.h>
@@ -7,35 +6,34 @@
 #include "wlr.h"
 
 // send a notification
-inline void notify_send(const std::string &format, ...) {
-    // get variadic
-    va_list args;
-    va_start(args, format);
-    char buffer[1024];
-    vsnprintf(buffer, sizeof(buffer), format.c_str(), args);
-    va_end(args);
-
-    // convert buffer to string
-    const std::string message = std::string(buffer);
+template <typename... Args>
+void notify_send(const std::string &format, Args... args) {
+    // format message
+    const int size_s = std::snprintf(nullptr, 0, format.c_str(), args...) + 1;
+    if (size_s <= 0)
+        throw std::runtime_error("Error during formatting.");
+    const auto size = static_cast<size_t>(size_s);
+    std::unique_ptr<char[]> buf(new char[size]);
+    std::snprintf(buf.get(), size, format.c_str(), args...);
+    const std::string message = std::string(buf.get(), buf.get() + size - 1);
 
     // log
-    wlr_log(WLR_ERROR, "%s", message.c_str());
+    wlr_log(WLR_INFO, "%s", message.c_str());
 
     // send notification
     if (fork() == 0) {
         execl("/bin/sh", "/bin/sh", "-c",
-              ("notify-send -a awm WARNING \"" + std::string(buffer) + "\"")
-                  .c_str(),
+              ("notify-send -a awm WARNING \"" + message + "\"").c_str(),
               nullptr);
     }
 }
 
+// stolen from https://stackoverflow.com/a/26221725
 template <typename... Args>
 std::string string_format(const std::string &format, Args... args) {
     int size_s = std::snprintf(nullptr, 0, format.c_str(), args...) + 1;
-    if (size_s <= 0) {
+    if (size_s <= 0)
         throw std::runtime_error("Error during formatting.");
-    }
     auto size = static_cast<size_t>(size_s);
     std::unique_ptr<char[]> buf(new char[size]);
     std::snprintf(buf.get(), size, format.c_str(), args...);
